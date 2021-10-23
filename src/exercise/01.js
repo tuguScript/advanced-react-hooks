a0e27535f14e195bf9f2e27c402176828c63879d
tuguscript <tuguscript@gmail.com>
2021-10-23 13:18:02 -0700

    01 extrea 2

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,14 +3,16 @@
 
 import * as React from 'react'
 
-function countReducer(state, newState) {
-  return newState
+function countReducer(state, action) {
+  return {...state, ...action}
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [count, setCount] = React.useReducer(countReducer, initialCount)
-  const increment = () => setCount(count + step)
-  return <button onClick={increment}>{count}</button>
+  const [state, setCount] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
+  const increment = () => setCount({count: state.count + step})
+  return <button onClick={increment}>{state.count}</button>
 }
 
 function App() {
